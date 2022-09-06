93296009d49bef43534241c66fd5a76a99e45d03
Brandon Lin <brandonlin9716@gmail.com>
2022-09-05 22:34:45 -0700

    Login & Signup frontend completed.

---
+++ app/src/main/java/com/example/weebchat/LoginFragment.kt
@@ -0,0 +1,43 @@
+package com.example.weebchat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.example.weebchat.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.loginFragment = this
+    }
+
+    fun login() {
+        Toast.makeText(context, "you logged in", Toast.LENGTH_SHORT).show()
+    }
+
+    fun signup() {
+        findNavController().navigate(R.id.action_loginFragment_to_signupFragment)
+    }
+}
---
+++ app/src/main/java/com/example/weebchat/SignupFragment.kt
@@ -0,0 +1,37 @@
+package com.example.weebchat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.weebchat.databinding.FragmentSignupBinding
+
+class SignupFragment : Fragment() {
+
+    private lateinit var binding: FragmentSignupBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_signup, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.signupFragment = this
+    }
+
+    fun signup() {
+        Toast.makeText(context, "you signed up", Toast.LENGTH_SHORT).show()
+    }
+}
